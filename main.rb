b0103a6458e6de84458b4f1d9cd769446d36e700
yy <yy@yy.com>
2013-11-15 18:31:01 +0800

    all fontground

--- main.rb
+++ main.rb
@@ -2,23 +2,55 @@
 require 'cgi'
 require 'erb'
 
+logfile = "/var/log/cgi-bin/sync_mc_or_music.log"
 
 lists = CGI.new['lists'].split("\s").select { |e| e =~ /^\d*$/ }.join(" ")
 
+result_shellcodes = %x[ ./main.sh '#{lists}' 2>&1 | tee #{logfile} ]
+
+
+
+sources_lists =%{
+
+Sync SQL(om_id) & FILES: 
+**************************************************************
+<%= lists %>
+
+
+
+
+RESULTS :
+**************************************************************
+<%= result_shellcodes%>
+
+}
+
+
+puts ERB.new(sources_lists).result(binding)
+
+
+
+=begin
 
 shellcodes = <<-header
-  ./main.sh '#{lists}'
+  ./main.sh '#{lists}' 2>&1
 header
 
-result_shellcodes = %x[ #{shellcodes} ]
+
+pid = spawn(
+  "#{shellcodes}", 
+  :out => "#{logfile}",
+  :err => "#{logfile}"
+)
+Process.detach(pid)
+
 
 sources_lists =%{
 
   Sync message:
 
-
   ==============================
-  <%= result_shellcodes %>
+  "Synchronization processed background......"
   ==============================
 
 
@@ -30,8 +62,11 @@ sources_lists =%{
   ==============================
   sql_om_id : <%= lists %>
   ==============================
-
 }
+
 puts ERB.new(sources_lists).result(binding)
+=end
+
+
 
 
